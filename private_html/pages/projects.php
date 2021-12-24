40a5eba30a80c9f448331107352d03f2d87ee935
OscarFreij <otg020313@gmai.com>
2021-12-24 22:46:32 +0100

    Added more projects and added Legend to page bottom

--- private_html/pages/projects.php
+++ private_html/pages/projects.php
@@ -3,15 +3,37 @@
 </div>
 <div id="project_Wrapper">
     <div status="done">
-        <h3>SSIS Lokaltrafiks Display <a href="https://ssis_ld.offthegridcg.me">LINK</a></h3>
-        <p>Display to show time tables for buss and subway stops/stations around NOD in Kista, Sweden</p>
-    </div>
-    <div status="done">
         <h3>UniPointCalc <a href="UniPointCalc/">LINK</a></h3>
         <p>Calculator for the ending score of gymnasium here in Sweden</p>
+    </div>
+    <div status="done">
+        <h3>SSIS Lokaltrafiks Display <a href="https://lokaltrafik.ssis.nu">LINK</a></h3>
+        <p>Display to show time tables for buss and subway stops/stations around NOD in Kista, Sweden</p>
     </div>
     <div status="done">
         <h3>Sequence The Game <a href="http://sequence.offthegridcg.me">LINK</a></h3>
         <p>A game I made during multiple streams on Twitch (<a href="https://twitch.tv/offthegridcg">OffTheGridCG</a>). More info on the site!</p>
     </div>
+    <div status="done">
+        <h3>Retune ReUse <a href="http://retunereuse.offthegridcg.me">LINK</a></h3>
+        <p>Retune ReUse is a internal auction site for the company I currently work on. This was made us to reuse old hardware from the company and put it to good use at home.</p>
+    </div>
+    <div status="active">
+        <h3>DDLCMods <a href="https://ddlcmods.offthegridcg.me">LINK</a></h3>
+        <p>DDLCMods is a website that will work as a database for Doki Doki Litrature Club (DDLC) mods. Authors will upload the mods here to help the comunity to have all the mods in one single space.</p>
+    </div>
+    <div status="planning">
+        <h3>PillTracker <a href="https://pilltracker.offthegridcg.me">LINK</a></h3>
+        <p>PillTracker is a tracker for your own convenience to keep track of you pills and when to take them.</p>
+    </div>
+</div>
+<div>
+    <p>
+        Legend:
+        <span style="color: green;">Done</span> | 
+        <span style="color: yellow;">On Hold</span> | 
+        <span style="color: cyan;">Active</span> | 
+        <span style="color: gainsboro;">Planning</span> | 
+        <span style="color: maroon;">Dropped</span>
+    </p>
 </div>
