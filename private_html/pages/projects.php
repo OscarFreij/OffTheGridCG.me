2763e3211ea8c41f2d3ab978d502127cb6e9771e
Oscar Freij <otg020313@gmail.com>
2023-12-15 02:25:39 +0100

    add items to .gitignore and update projects page

--- private_html/pages/projects.php
+++ private_html/pages/projects.php
@@ -11,20 +11,27 @@
         <p>Display to show time tables for buss and subway stops/stations around NOD in Kista, Sweden</p>
     </div>
     <div status="done">
-        <h3>Sequence The Game <a href="http://sequence.offthegridcg.me">LINK</a></h3>
+        <h3>Sequence The Game <a href="https://github.com/OscarFreij/Sequence">LINK (GAME)</a> <a href="https://github.com/OscarFreij/Sequence_Server">LINK (SRV)</a></h3>
         <p>A game I made during multiple streams on Twitch (<a href="https://twitch.tv/offthegridcg">OffTheGridCG</a>). More info on the site!</p>
+        <p><b>NOTE:</b> Score server is permanently decommissioned.</p>
     </div>
     <div status="done">
-        <h3>Retune ReUse <a href="http://retunereuse.offthegridcg.me">LINK</a></h3>
-        <p>Retune ReUse is a internal auction site for the company I currently work on. This was made us to reuse old hardware from the company and put it to good use at home.</p>
+        <h3>Retune ReUse <a href="https://github.com/OscarFreij/ReUse">LINK</a></h3>
+        <p>Retune ReUse is a internal auction site for the company I currently work on. This was made as a tool to resell old hardware from the company and put it to good use at home.</p>
     </div>
-    <div status="active">
+    <div status="dropped">
         <h3>DDLCMods <a href="https://ddlcmods.offthegridcg.me">LINK</a></h3>
         <p>DDLCMods is a website that will work as a database for Doki Doki Litrature Club (DDLC) mods. Authors will upload the mods here to help the comunity to have all the mods in one single space.</p>
+        <p><b>DROP REASON:</b> another page filled the void.</p>
     </div>
-    <div status="planning">
+    <div status="dropped">
         <h3>PillTracker <a href="https://pilltracker.offthegridcg.me">LINK</a></h3>
         <p>PillTracker is a tracker for your own convenience to keep track of you pills and when to take them.</p>
+        <p><b>DROP REASON:</b> potential issues with GDPR laws regarding storrage of medical data.</p>
+    </div>
+    <div status="active">
+        <h3>CMTrace Online Tool <a href="https://cmtot.offthegridcg.me">LINK</a></h3>
+        <p>CMTrace Online Tool or "CRTOT" is an online log viewer. To review logs from applications and services like Powershell Application Deployment Toolkit "PSADT" and Intune Management Extension "IME"</p>
     </div>
 </div>
 <div>
