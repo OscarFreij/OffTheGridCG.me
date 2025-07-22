9c1239b2a094a839affd5c6ef536bdf9173f1b10
Oscar Freij <otg020313@gmail.com>
2025-07-22 03:29:17 +0200

    finish new version

---
+++ private_html/pages/contact.php
@@ -0,0 +1,14 @@
+<div>
+    <header>
+        <h1>Contact - How to get in contact with me</h1>
+    </header>
+    <p>If you have any questions, feedback, or just want to say hello, feel free to reach out through any of the following methods:</p>
+    <ul>
+        <li>LinkedIn: <a href="https://www.linkedin.com/in/oscar-freij/">Oscar Freij</a></li>
+        <li>GitHub: <a href="https://github.com/OscarFreij">OscarFreij</a></li>
+        <li>Discord: Ogglas</li>
+    </ul>
+    <p>Contact me through any of the above methods, and I will get back to you as soon as possible.</p>
+    <p>Thank you for visiting my site, and I hope to hear from you soon!</p>
+    <p>If you want to contact me through email or phone, that is not possible at this time. I prefer to keep that part of my contact information private for now.</p>
+</div>
