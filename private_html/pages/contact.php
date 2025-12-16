7a1f5d4fa86849858cbc99438f96cb8d10cac94f
Oscar Freij <otg020313@gmail.com>
2025-12-16 03:19:33 +0100

    refactor contact page

--- private_html/pages/contact.php
+++ private_html/pages/contact.php
@@ -2,14 +2,16 @@
     <header>
         <h1>Contact - How to get in contact with me</h1>
     </header>
-    <p>If you have any questions, feedback, or just want to say hello, feel free to reach out through any of the following methods:</p>
-    <ul>
-        <li>LinkedIn: <a href="https://www.linkedin.com/in/oscar-freij-4a5690165/">Oscar Freij</a></li>
-        <li>GitHub: <a href="https://github.com/OscarFreij">OscarFreij</a></li>
-        <li>Discord: Ogglas</li>
-        <li>Instagram: <a href="https://instagram.com/ogglas_02">ogglas_02</a></li>
-    </ul>
-    <p>Contact me through any of the above methods, and I will get back to you as soon as possible.</p>
-    <p>Thank you for visiting my site, and I hope to hear from you soon!</p>
-    <p>If you want to contact me through email or phone, that is not possible at this time. I prefer to keep that part of my contact information private for now.</p>
+    <main>
+        <p>If you have any questions, feedback, or just want to say hello, feel free to reach out through any of the following methods:</p>
+        <ul class="contact-list">
+            <li>LinkedIn: <a href="https://www.linkedin.com/in/oscar-freij-4a5690165/">Oscar Freij</a></li>
+            <li>GitHub: <a href="https://github.com/OscarFreij">OscarFreij</a></li>
+            <li>Discord: Ogglas</li>
+            <li>Instagram: <a href="https://instagram.com/ogglas_02">ogglas_02</a></li>
+        </ul>
+        <p>Contact me through any of the above methods, and I will get back to you as soon as possible.</p>
+        <p>Thank you for visiting my site, and I hope to hear from you soon!</p>
+        <p>If you want to contact me through email or phone, that is not possible at this time. I prefer to keep that part of my contact information private for now.</p>
+    </main>
 </div>
