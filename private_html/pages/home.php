97b681058f273b7507bd6583d6ede891d99f4fc1
Oscar Freij <otg020313@gmail.com>
2025-12-16 03:29:03 +0100

    refactor homepage again

--- private_html/pages/home.php
+++ private_html/pages/home.php
@@ -3,17 +3,21 @@
         <h1>Hello there and welcome!</h1>
     </header>
     <main>
-        <div id="preamble-text">
-            <h2>This is my personal website, where I share information about myself, my projects, and my interests.</p>
-            <h3>There are a few ways may have sumbled upon this page, either:</h3>
+        <div class="bg-box-1">
+            <div id="preamble-text">
+                <h2>This is my personal website, where I share information about myself, my projects, and my interests.</p>
+                <h3>There are a few ways may have sumbled upon this page, either:</h3>
+            </div>
+            <div id="welcome-list-display">
+                <span></span>
+            </div>
         </div>
-        <div id="welcome-list-display">
-            <span></span>
-        </div>
-        <div id="closing-text">
-            <p>Regardless of how you got here, I hope you find what you are looking for!</p>
-            <p>Feel free to explore the site, and if you have any questions or feedback, don't hesitate to reach out through the contact page.</p>
-            <p>Thank you for visiting my site, and I hope you have a great day!</p>
+        <div class="bg-box-2">
+            <div id="closing-text">
+                <p>Regardless of how you got here, I hope you find what you are looking for!</p>
+                <p>Feel free to explore the site, and if you have any questions or feedback, don't hesitate to reach out through the contact page.</p>
+                <p>Thank you for visiting my site, and I hope you have a great day!</p>
+            </div>
         </div>
     </main>
 </div>
